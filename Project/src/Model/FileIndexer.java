af3823bacea72909bab72cf34d636ca29498508f
ppapahr <34003820+ppapahr@users.noreply.github.com>
2021-05-26 16:31:40 +0300

    indexer fixes

--- Project/src/Model/FileIndexer.java
+++ Project/src/Model/FileIndexer.java
@@ -1,14 +1,9 @@
 package Model;
 
-import java.io.FileFilter;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.InputStreamReader;
-import java.nio.file.Files;
 import java.nio.file.Paths;
 import org.apache.lucene.index.CorruptIndexException;
 import org.apache.lucene.index.IndexWriter;
