418510520809fb1bc1f75e6dc792063d860e129b
Pgiannako <43864555+Pgiannako@users.noreply.github.com>
2021-05-27 20:39:14 +0300

    Button Abstract Class and Search button Extender
    Initial commit of button functionality, needs work

--- Project/src/View/SearchButton.java
+++ Project/src/View/SearchButton.java
@@ -1,5 +1,20 @@
 package View;
 
-public class SearchButton {
+import java.awt.Dimension;
+import java.awt.Font;
 
+import javax.swing.JButton;
+
+public class SearchButton extends Button {
+	
+	public SearchButton(int width, int height) {
+
+		this.button = new JButton("Search");
+		button.setFont(new Font("Arial", Font.PLAIN, 16));
+		button.setPreferredSize(new Dimension(width, height));
+	}
+	
+	public JButton getButton() {
+		return this.button;
+	}
 }
